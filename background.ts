896085d0b03d3d903e1ae73993398c055ebcf780
Ilya Barinov <ilya.bv.2002@bk.ru>
2024-06-08 16:53:36 +0300

    save

--- background.ts
+++ background.ts
@@ -1,268 +1,16 @@
-// import axios from "axios";
-// import { persistor, store } from '~store';
-// import { setFeedback, setTimer } from '~feedbackSlice';
-
-// const getCookies = (): Promise<string> => {
-//   return new Promise((resolve, reject) => {
-//     chrome.cookies.getAll({}, (cookies) => {
-//       if (chrome.runtime.lastError) {
-//         reject(chrome.runtime.lastError);
-//       } else {
-//         const cookieString = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-//         resolve(cookieString);
-//       }
-//     });
-//   });
-// };
-
-// const getSettings = (): Promise<{ apiUrl: string; companyId: string; headerApiKey: string }> => {
-//   return new Promise((resolve, reject) => {
-//     chrome.storage.local.get(["ApiUrl", "company_id", "HeaderApiKey"], (result) => {
-//       if (chrome.runtime.lastError) {
-//         reject(chrome.runtime.lastError);
-//       } else {
-//         const apiUrl = result.ApiUrl || "";
-//         const companyId = result.company_id || "";
-//         const headerApiKey = result.HeaderApiKey || "";
-//         resolve({
-//           apiUrl,
-//           companyId,
-//           headerApiKey
-//         });
-//       }
-//     });
-//   });
-// };
-
-// const sendItemsBatch = async (reviews) => {
-//   const { apiUrl, headerApiKey } = await getSettings();
-//   const data = reviews
-//     .filter(review => review.interaction_status !== "PROCESSED")
-//     .map(review => ({
-//       prod_art: review.product.offer_id,
-//       prod_name: review.product.title,
-//       feedback_ID: review.uuid,
-//       rating: review.rating,
-//       positive: review.text.positive,
-//       negative: review.text.negative,
-//       comment: review.text.comment,
-//       market: "ozon",
-//       dateTimeFeedback: review.published_at
-//     }));
-
-//   if (data.length > 0) {
-//     try {
-//       await axios.post(`${apiUrl}/feedbacks/add-feedbacks/`, { feedbacks: data }, {
-//         headers: {
-//           "HeaderApiKey": headerApiKey
-//         }
-//       });
-//     } catch (err) {
-//     }
-//   } else {
-//   }
-// };
-
-// const getFeedback = async () => {
-//   try {
-//     const cookies = await getCookies();
-//     const { companyId } = await getSettings();
-//     let pagination_last_timestamp = null;
-//     let pagination_last_uuid = null;
-//     let allReviews = [];
-//     let unprocessedReviews = [];
-
-//     while (true) {
-//       const response = await fetch("https://seller.ozon.ru/api/v3/review/list", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           "Cookie": cookies,
-//         },
-//         body: JSON.stringify({
-//           "with_counters": false,
-//           "sort": { "sort_by": "PUBLISHED_AT", "sort_direction": "DESC" },
-//           "company_type": "seller",
-//           "filter": { "interaction_status": ["NOT_VIEWED"] },
-//           "company_id": companyId,
-//           "pagination_last_timestamp": pagination_last_timestamp,
-//           "pagination_last_uuid": pagination_last_uuid
-//         })
-//       });
-
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-
-//       const data = await response.json();
-//       allReviews = [...allReviews, ...data.result];
-
-//       const newUnprocessed = data.result.filter(item => item.interaction_status !== 'PROCESSED');
-//       unprocessedReviews = [...unprocessedReviews, ...newUnprocessed];
-
-//       pagination_last_timestamp = data.pagination_last_timestamp;
-//       pagination_last_uuid = data.pagination_last_uuid;
-
-//       if (!pagination_last_timestamp || !pagination_last_uuid) {
-//         break;
-//       }
-//     }
-
-//     while (true) {
-//       const response = await fetch("https://seller.ozon.ru/api/v3/review/list", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           "Cookie": cookies,
-//         },
-//         body: JSON.stringify({
-//           "with_counters": false,
-//           "sort": { "sort_by": "PUBLISHED_AT", "sort_direction": "DESC" },
-//           "company_type": "seller",
-//           "filter": { "interaction_status": ["VIEWED"] },
-//           "company_id": companyId,
-//           "pagination_last_timestamp": pagination_last_timestamp,
-//           "pagination_last_uuid": pagination_last_uuid
-//         })
-//       });
-
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-
-//       const data = await response.json();
-//       allReviews = [...allReviews, ...data.result];
-
-//       const newUnprocessed = data.result.filter(item => item.interaction_status !== 'PROCESSED');
-//       unprocessedReviews = [...unprocessedReviews, ...newUnprocessed];
-
-//       pagination_last_timestamp = data.pagination_last_timestamp;
-//       pagination_last_uuid = data.pagination_last_uuid;
-
-//       if (!pagination_last_timestamp || !pagination_last_uuid) {
-//         break;
-//       }
-//     }
-
-//     store.dispatch(setFeedback(unprocessedReviews.length));
-//     sendItemsBatch(unprocessedReviews);
-//   } catch (error) {
-//   }
-// };
-
-// const ansverRiviev = async (review_uuid, text) => {
-//   const { companyId } = await getSettings();
-//   const data = {
-//     "review_uuid": review_uuid,
-//     "text": text,
-//     "company_type": "seller",
-//     "company_id": companyId
-//   };
-//   try {
-//     const cookies = await getCookies();
-//     const response = await fetch("https://seller.ozon.ru/api/review/comment/create", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Cookie": cookies,
-//       },
-//       body: JSON.stringify(data)
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-
-//     const result = await response.json();
-
-//     return true;
-
-//   } catch (error) {
-//     console.log(error)
-//     return false;
-//   }
-// };
-
-// const updateFeedbackStatus = async (feedbackId, status) => {
-//   console.log(status)
-//   const { apiUrl, headerApiKey } = await getSettings();
-//   try {
-//     const response = await axios.post(`${apiUrl}/feedbacks/change-status-feedbacks/`, {
-//       feedbackId: feedbackId,
-//       status: status
-//     }, {
-//       headers: {
-//         "HeaderApiKey": headerApiKey
-//       }
-//     });
-//     if (response.status !== 200) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//   } catch (error) {
-//   }
-// };
-
-// const processFeedbacks = async () => {
-//   const { apiUrl, headerApiKey } = await getSettings();
-//   try {
-//     const response = await axios.get(`${apiUrl}/feedbacks/get-feedbacks-to-markets/?market=ozon`, {
-//       headers: {
-//         "HeaderApiKey": headerApiKey
-//       }
-//     });
-
-//     if (response.status !== 200) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-
-//     const { data } = response.data;
-//     const results = await Promise.all(data.map(async (feedback) => {
-//       const success = await ansverRiviev(feedback.feedback_ID, feedback.ansver);
-//       const status = success ? "success" : "error";
-//       await updateFeedbackStatus(feedback.id, status);
-//       return success;
-//     }));
-//   } catch (error) {
-//   }
-// };
-
-// const startIntervals = async () => {
-//   const state = store.getState();
-//   const interval_time = state.feedback.interval;
-//   const work = state.feedback.work;
-//   console.log("Work status", work);
-//   if (work) {
-//     const newTimer = state.feedback.timer > 1 ? state.feedback.timer - 1 : interval_time;
-//     store.dispatch(setTimer(newTimer));
-//     if (newTimer === 1) {
-//       await getFeedback();
-//       await processFeedbacks()
-//     }
-//   } else {
-//     store.dispatch(setTimer(interval_time));
-//   }
-// };
-
-// chrome.alarms.create('feedbackInterval', { periodInMinutes: 0.01 });
-
-// chrome.alarms.onAlarm.addListener((alarm) => {
-//   if (alarm.name === 'feedbackInterval') {
-//     startIntervals()
-//   }
-// });
-
-
 import axios from "axios";
 
 interface StorageResult {
   work?: boolean;
   timer?: number;
+  interval?: number;
 }
 
 interface Settings {
   apiUrl: string;
   companyId: string;
   headerApiKey: string;
+  interval: number;
 }
 
 const getCookies = (): Promise<string> => {
@@ -278,26 +26,26 @@ const getCookies = (): Promise<string> => {
   });
 };
 
-
 const getSettings = (): Promise<Settings> => {
   return new Promise((resolve, reject) => {
-    chrome.storage.local.get(["ApiUrl", "company_id", "HeaderApiKey"], (result) => {
+    chrome.storage.local.get(["ApiUrl", "company_id", "HeaderApiKey", "interval"], (result) => {
       if (chrome.runtime.lastError) {
         reject(chrome.runtime.lastError);
       } else {
         const apiUrl = result.ApiUrl || "";
         const companyId = result.company_id || "";
         const headerApiKey = result.HeaderApiKey || "";
+        const interval = result.interval || 60;  // Интервал по умолчанию 60 секунд
         resolve({
           apiUrl,
           companyId,
-          headerApiKey
+          headerApiKey,
+          interval
         });
       }
     });
   });
 };
-
 
 const sendItemsBatch = async (reviews) => {
   const { apiUrl, headerApiKey } = await getSettings();
@@ -491,21 +239,20 @@ const processFeedbacks = async () => {
 };
 
 const checkAndProcessFeedbacks = async () => {
-  const { work, timer } = await new Promise<StorageResult>((resolve) => {
-    chrome.storage.local.get(["work", "timer"], (result) => resolve(result as StorageResult));
+  const { work, timer, interval } = await new Promise<StorageResult>((resolve) => {
+    chrome.storage.local.get(["work", "timer", "interval"], (result) => resolve(result as StorageResult));
   });
-
-  console.log(work)
+  console.log("[ ? ] WorkStatus ", work)
   if (work) {
-    let newTimer = timer > 1 ? timer - 1 : 60;
+    let newTimer = timer > 1 ? timer - 1 : interval;
     chrome.storage.local.set({ timer: newTimer });
     if (newTimer === 1) {
-      console.log(123)
-      // await getFeedback();
-      // await processFeedbacks();
+      await getFeedback();
+      await processFeedbacks();
     }
   } else {
-    chrome.storage.local.set({ timer: 60 });
+
+    chrome.storage.local.set({ timer: interval });
   }
 };
 
@@ -518,5 +265,17 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 });
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ timer: 60, work: false });
+  chrome.storage.local.set({ timer: 60, work: false, interval: 60, feedback: 0 });
+});
+
+
+chrome.storage.onChanged.addListener(async (changes, areaName) => {
+  if (areaName === "local" && changes.work) {
+    const oldValue = changes.work.oldValue;
+    const newValue = changes.work.newValue;
+    if (oldValue === false && newValue === true) {
+      await getFeedback();
+      await processFeedbacks();
+    }
+  }
 });
--- popup.tsx
+++ popup.tsx
@@ -1,196 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "@plasmohq/redux-persist/integration/react";
-// import { store, persistor, useAppDispatch, useAppSelector } from "./store";
-// import { setWork } from "./feedbackSlice";
-// import { Tabs, Switch, Input, Button, Form, Typography, message } from "antd";
-// import axios from "axios";
-
-// const { TabPane } = Tabs;
-// const { Text } = Typography;
-
-// const Popup = () => {
-//   const dispatch = useAppDispatch();
-//   const timer = useAppSelector((state) => state.feedback.timer);
-//   const data = useAppSelector((state) => state.feedback.feedback);
-//   const work = useAppSelector((state) => state.feedback.work);
-
-//   const [url, setUrl] = useState("");
-//   const [apiUrl, setApiUrl] = useState("");
-//   const [companyId, setCompanyId] = useState("");
-//   const [headerApiKey, setHeaderApiKey] = useState("");
-//   const [apiUrlError, setApiUrlError] = useState("");
-//   const [companyIdError, setCompanyIdError] = useState("");
-
-//   useEffect(() => {
-//     const queryInfo = { active: true, currentWindow: true };
-//     chrome.tabs.query(queryInfo, (tabs) => {
-//       const tab = tabs[0];
-//       setUrl(tab.url);
-//     });
-
-//     chrome.storage.local.get(["ApiUrl", "company_id", "HeaderApiKey"], (result) => {
-//       if (result.ApiUrl) setApiUrl(result.ApiUrl);
-//       if (result.company_id) setCompanyId(result.company_id);
-//       if (result.HeaderApiKey) setHeaderApiKey(result.HeaderApiKey);
-//     });
-//   }, []);
-
-//   const validateApiUrl = (url) => {
-//     try {
-//       const parsedUrl = new URL(url);
-//       return parsedUrl.protocol === "http:" || parsedUrl.protocol === "https:";
-//     } catch (e) {
-//       return false;
-//     }
-//   };
-
-//   const validateCompanyId = (id) => {
-//     return /^\d+$/.test(id);
-//   };
-
-//   const handleSaveSettings = async () => {
-//     let valid = true;
-
-//     if (!validateApiUrl(apiUrl)) {
-//       setApiUrlError("Введите корректный URL");
-//       valid = false;
-//     } else {
-//       let cleanedApiUrl = apiUrl;
-//       if (apiUrl.endsWith("/")) {
-//         cleanedApiUrl = apiUrl.slice(0, -1);
-//       }
-//       setApiUrl(cleanedApiUrl);
-//       setApiUrlError("");
-//     }
-
-//     if (!validateCompanyId(companyId)) {
-//       setCompanyIdError("Company ID должно состоять только из чисел");
-//       valid = false;
-//     } else {
-//       setCompanyIdError("");
-//     }
-
-//     if (valid) {
-//       try {
-//         const response = await axios.get(`${apiUrl}/feedbacks/chek-ozon-plagin-settings`, {
-//           headers: {
-//             "HeaderApiKey": headerApiKey,
-//           },
-//         });
-//         if (response.status === 200 && response.data.status) {
-//           chrome.storage.local.set({
-//             ApiUrl: apiUrl,
-//             company_id: companyId,
-//             HeaderApiKey: headerApiKey
-//           }, () => {
-//             message.success("Настройки сохранены");
-//           });
-//         } else {
-//           message.error("Ошибка при проверке настроек");
-//         }
-//       } catch (error) {
-//         if (error.response && error.response.status === 401) {
-//           message.error("Неверный токен");
-//         } else {
-//           message.error(`Ошибка при проверке настроек: ${error.message}`);
-//         }
-//       }
-//     }
-//   };
-
-//   const handleRedirect = () => {
-//     chrome.tabs.create({ url: "https://seller.ozon.ru/app" });
-//   };
-
-//   function formatSecondsToMMSS(seconds: number) {
-//     const minutes = Math.floor(seconds / 60);
-//     const remainingSeconds = seconds % 60;
-//     return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
-//   }
-
-//   return (
-//     <Tabs defaultActiveKey="1">
-//       <TabPane tab="Работа" key="1">
-//         {url.includes("https://seller.ozon.ru/app") ? (
-//           <>
-//             {url.includes("registration") ? (
-//               <Text>Для работы плагина необходимо авторизоваться</Text>
-//             ) : (
-//               <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", gap: 10 }}>
-//                 {apiUrl && companyId && headerApiKey ? (
-//                   <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 10 }}>
-//                     <Text style={{ fontSize: 14 }}>{work ? "Включено" : "Выключено"}</Text>
-//                     <Switch checked={work} onChange={() => { dispatch(setWork(!work)) }} />
-//                   </div>
-//                 ) : (
-//                   <Text>Для работы плагина выполните настройку</Text>
-//                 )}
-//                 {work && (
-//                   <>
-//                     <Text>Следующее обновление через - {formatSecondsToMMSS(timer)}</Text>
-//                     <Text>Необработано отзывов - {data}</Text>
-//                   </>
-//                 )}
-//               </div>
-//             )}
-//           </>
-//         ) : (
-//           <div>
-//             <Text>Данный плагин работает только на <a onClick={handleRedirect} style={{ color: 'blue', cursor: 'pointer' }}>этой странице</a>.</Text>
-//           </div>
-//         )}
-//       </TabPane>
-//       <TabPane tab="Настройки" key="2">
-//         <Form layout="vertical" style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-//           <Form.Item label="ApiUrl" validateStatus={apiUrlError ? "error" : ""} help={apiUrlError}>
-//             <Input
-//               placeholder="ApiUrl"
-//               value={apiUrl}
-//               onChange={(e) => setApiUrl(e.target.value)}
-//             />
-//           </Form.Item>
-//           <Form.Item label="Company ID" validateStatus={companyIdError ? "error" : ""} help={companyIdError}>
-//             <Input
-//               placeholder="Company ID"
-//               value={companyId}
-//               onChange={(e) => setCompanyId(e.target.value)}
-//             />
-//           </Form.Item>
-//           <Form.Item label="Header Api Key">
-//             <Input
-//               placeholder="Header Api Key"
-//               value={headerApiKey}
-//               onChange={(e) => setHeaderApiKey(e.target.value)}
-//             />
-//           </Form.Item>
-//           <Form.Item>
-//             <Button type="primary" onClick={handleSaveSettings}>Сохранить</Button>
-//           </Form.Item>
-//         </Form>
-//       </TabPane>
-//     </Tabs>
-//   );
-// };
-
-// function IndexPopup() {
-//   return (
-//     <div style={{ width: 300, background: "radial-gradient(139.34% 274.47% at 106.09% 320.4%,rgba(249,17,128,.15) 0,rgba(249,17,85,0) 100%),linear-gradient(339deg,rgba(223,198,255,.64),rgba(235,236,255,.23))", margin: -20 }}>
-//       <Provider store={store}>
-//         <PersistGate loading={null} persistor={persistor}>
-//           <div style={{ margin: 20, padding: 10 }}>
-//             <h2>Feedbacks Ozon Answer</h2>
-//             <Popup />
-//           </div>
-//         </PersistGate>
-//       </Provider>
-//     </div>
-//   );
-// }
-
-// export default IndexPopup;
-
-
 import { useEffect, useState } from "react";
 import { Tabs, Switch, Input, Button, Form, Typography, message } from "antd";
 import axios from "axios";
@@ -208,6 +15,8 @@ const Popup = () => {
   const [work, setWork] = useState(false);
   const [timer, setTimer] = useState(0);
   const [feedback, setFeedback] = useState(0);
+  const [interval, setInterval] = useState(60);  // Добавляем состояние для интервала
+  const [intervalError, setIntervalError] = useState("");
 
   useEffect(() => {
     const queryInfo = { active: true, currentWindow: true };
@@ -216,13 +25,14 @@ const Popup = () => {
       setUrl(tab.url);
     });
 
-    chrome.storage.local.get(["ApiUrl", "company_id", "HeaderApiKey", "work", "timer", "feedback"], (result) => {
+    chrome.storage.local.get(["ApiUrl", "company_id", "HeaderApiKey", "work", "timer", "feedback", "interval"], (result) => {
       if (result.ApiUrl) setApiUrl(result.ApiUrl);
       if (result.company_id) setCompanyId(result.company_id);
       if (result.HeaderApiKey) setHeaderApiKey(result.HeaderApiKey);
       if (result.work !== undefined) setWork(result.work);
       if (result.timer !== undefined) setTimer(result.timer);
       if (result.feedback !== undefined) setFeedback(result.feedback);
+      if (result.interval !== undefined) setInterval(result.interval);  // Получаем интервал из хранилища
     });
 
     // Add listener to storage changes
@@ -236,6 +46,9 @@ const Popup = () => {
         }
         if (changes.work) {
           setWork(changes.work.newValue);
+        }
+        if (changes.interval) {
+          setInterval(changes.interval.newValue);
         }
       }
     };
@@ -260,6 +73,24 @@ const Popup = () => {
     return /^\d+$/.test(id);
   };
 
+  const validateInterval = (value) => {
+    const parsedValue = parseInt(value, 10);
+    if (!isNaN(parsedValue) && parsedValue >= 1 && parsedValue <= 60 * 20) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const handleIntervalChange = (value) => {
+    if (validateInterval(value)) {
+      setIntervalError("");
+      setInterval(parseInt(value, 10));
+    } else {
+      setIntervalError("Интервал должен быть числом от 10 до 1200");
+    }
+  };
+
   const handleSaveSettings = async () => {
     let valid = true;
 
@@ -282,6 +113,13 @@ const Popup = () => {
       setCompanyIdError("");
     }
 
+    if (!validateInterval(interval.toString())) {
+      setIntervalError("Интервал должен быть числом от 10 до 1200");
+      valid = false;
+    } else {
+      setIntervalError("");
+    }
+
     if (valid) {
       try {
         const response = await axios.get(`${apiUrl}/feedbacks/chek-ozon-plagin-settings`, {
@@ -293,7 +131,8 @@ const Popup = () => {
           chrome.storage.local.set({
             ApiUrl: apiUrl,
             company_id: companyId,
-            HeaderApiKey: headerApiKey
+            HeaderApiKey: headerApiKey,
+            interval
           }, () => {
             message.success("Настройки сохранены");
           });
@@ -384,10 +223,20 @@ const Popup = () => {
                   onChange={(e) => setHeaderApiKey(e.target.value)}
                 />
               </Form.Item>
+              <Form.Item label="Интервал (в секундах)" validateStatus={intervalError ? "error" : ""} help={intervalError}>
+                <Input
+                  placeholder="Интервал"
+                  value={interval.toString()}
+                  onChange={(e) => handleIntervalChange(e.target.value)}
+                />
+              </Form.Item>
               <Form.Item>
-                <Button type="primary" onClick={handleSaveSettings}>Сохранить</Button>
+                <Button type="primary" style={{ flex: 1, width: "100%" }} onClick={handleSaveSettings}>Сохранить</Button>
               </Form.Item>
             </Form>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", opacity: 0.3 }}>
+              <p>version 0.2.0</p>
+            </div>
           </TabPane>
         </Tabs>
       </div>
